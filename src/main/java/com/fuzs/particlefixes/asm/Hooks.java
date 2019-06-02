a9089bc67368adf04757515b2c80e7bf745ead9a
Fuzss <MrFusseel@gmail.com>
2019-06-02 10:51:39 +0200

    Fixed a NetworkDispatcher exception

--- src/main/java/com/fuzs/particlefixes/asm/Hooks.java
+++ src/main/java/com/fuzs/particlefixes/asm/Hooks.java
@@ -1,7 +1,6 @@
 package com.fuzs.particlefixes.asm;
 
 import net.minecraft.util.EnumParticleTypes;
-import net.minecraft.world.World;
 import net.minecraft.world.WorldServer;
 
 import java.util.Objects;
@@ -10,7 +9,18 @@ import java.util.Objects;
 public final class Hooks {
 
     public static void spawnServerParticle(WorldServer world, int particleID, boolean ignoreRange, double xCoord, double yCoord, double zCoord, double xSpeed, double ySpeed, double zSpeed, int... parameters) {
-            world.spawnParticle(Objects.requireNonNull(EnumParticleTypes.getParticleFromId(particleID)), xCoord, yCoord, zCoord, 1, 0.0D, 0.0D, 0.0D, (xSpeed * ySpeed * zSpeed) / 3.0D, parameters);
+
+        EnumParticleTypes particle = Objects.requireNonNull(EnumParticleTypes.getParticleFromId(particleID));
+
+        // this check is necessary as in SPacketParticles there are always as many ints written to the PacketBuffer as there are arguments pre-defined for that particle type
+        // problem is though, that there is no check in vanilla if there are actually as many parameters passed as there are required which will lead to an ArrayIndexOutOfBoundsException
+        // vanilla would even run into that bug itself when executing EntityLivingBase#updateItemUse on the server when there are no sub-types for an item, this only doesn't happen due to MC-10369 which this mod fixes
+        if (parameters.length == particle.getArgumentCount()) {
+
+            world.spawnParticle(particle, xCoord, yCoord, zCoord, 1, 0, 0, 0, (xSpeed * ySpeed * zSpeed) / 3.0, parameters);
+
+        }
+
     }
 
 }
