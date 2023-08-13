7fa43836ae7e64e5bc9258b7fbd5712a64f84c67
Faboslav <fabianek.ales@gmail.com>
2023-08-13 20:45:00 +0200

    Added friendsandfoes:mauler to the blacklist

--- Common/src/main/java/fuzs/respawninganimals/config/ServerConfig.java
+++ Common/src/main/java/fuzs/respawninganimals/config/ServerConfig.java
@@ -11,7 +11,7 @@ import java.util.List;
 
 public class ServerConfig implements ConfigCore {
     @Config(name = "animal_blacklist", description = {"Blacklist animals which will never despawn.", ConfigDataSet.CONFIG_DESCRIPTION})
-    List<String> animalBlacklistRaw = Lists.newArrayList("whisperwoods:hirschgeist");
+    List<String> animalBlacklistRaw = Lists.newArrayList("whisperwoods:hirschgeist", "friendsandfoes:mauler");
     @Config(name = "animal_mob_cap", description = "Constant for determining when to stop spawning animals in a world. Normally set to 10, monster constant is 70 for comparison. 18 is chosen to mimic spawning mechanics of the beta era.")
     public int maxAnimalNumber = 18;
     @Config(name = "summoned_mob_persistence", description = "Make all mobs (not just animals) automatically persistent when spawned using the \"/summon\" command, a spawn egg or a dispenser.")
